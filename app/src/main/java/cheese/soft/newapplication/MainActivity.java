f34085a26bb7999490fb1da7abfa9a6e6603ff18
Cheese <svvcheese@gmail.com>
2018-10-02 17:32:24 +0300

    Add 2 button

--- app/src/main/java/cheese/soft/newapplication/MainActivity.java
+++ app/src/main/java/cheese/soft/newapplication/MainActivity.java
@@ -2,6 +2,7 @@ package cheese.soft.newapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button);
     }
 }
